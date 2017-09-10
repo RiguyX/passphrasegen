f0735c6fdfa0193fd45fe9a05bed8760c29fa8d7
Riley <riguyx2@gmail.com>
2017-09-10 18:57:13 -0500

    Removed character-based aspects of the generator.
    Commented out definitions of vars minchar and maxchar and removed references to them.

--- generator.py
+++ generator.py
@@ -3,12 +3,12 @@ import secrets
 #Allow user to specify minimum and maximum lengths of passphrase and ensure specifications are valid
 #todo: Allow user to specify spaces between words
 while 1==1:
-  minchar = int(input("Minimum Character Length: "))
-  maxchar = int(input("Maximum Character Length: "))
+#  minchar = int(input("Minimum Character Length: "))
+#  maxchar = int(input("Maximum Character Length: "))
   minword = int(input("Minimum Word Length: "))
   maxword = int(input("Maximum Word Length: "))
   separators = input("Seperators (Multiple Allowed): ")
-  if (minchar <= maxchar) and (minword <= maxword) and (minchar > 0) and (minword > 0):
+  if (minword <= maxword) and (minword > 0):
     break
   else:
     print("Input invalid! Make sure minimum lengths are shorter than maximums and are positive numbers!")
