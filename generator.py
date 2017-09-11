3f223e758c3771cf9661aad8db044752ff81fa56
Riley <riguyx2@gmail.com>
2017-09-10 19:16:44 -0500

    Updated todo
    Removed outdated todo comments

--- generator.py
+++ generator.py
@@ -1,7 +1,6 @@
 import random
 import secrets
 #Allow user to specify minimum and maximum lengths of passphrase and ensure specifications are valid
-#todo: Allow user to specify spaces between words
 while 1==1:
 #  minchar = int(input("Minimum Character Length: "))
 #  maxchar = int(input("Maximum Character Length: "))
@@ -14,7 +13,7 @@ while 1==1:
     print("Input invalid! Make sure minimum lengths are shorter than maximums and are positive numbers!")
     continue
 print("Input valid. The actual generator is a work in progress, so DON'T USE THIS PASSPHRASE!")
-#todo: Use an actual dictionary source for words, implement character length checker, add space characters, add numbers
+#todo: Use an actual dictionary source for words
 passphrase = ""
 for i in range(random.randrange(minword, maxword)):
   word = secrets.choice(["Agent","Blurt","Center","Difficult","Elite","Forearm","Guerilla"]) #THIS IS ONLY AN EXPERIMENTAL LIST!! Do not use this to generate passphrases!
