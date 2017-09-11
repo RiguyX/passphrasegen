c9418146feaa22d7931ee45759cfa43df3d389af
Riley <riguyx2@gmail.com>
2017-09-10 21:10:17 -0500

    Update generator.py
    Fixed some things with option processing.

--- generator.py
+++ generator.py
@@ -8,43 +8,53 @@ while 1==1:
   3) Exit the program?""")
   userchoice = input("(Please select 1, 2, or 3): ")
   if userchoice == "1":
-    #Allow user to specify minimum and maximum lengths of passphrase and ensure specifications are valid
-    while 1==1:
-    #  Character length management is currently a disabled feature.
-    #  minchar = int(input("Minimum Character Length: "))
-    #  maxchar = int(input("Maximum Character Length: "))
-      minword = int(input("Minimum Word Length: "))
-      maxword = int(input("Maximum Word Length: "))
-      separators = input("Seperators (Multiple Allowed): ")
-      if (minword <= maxword) and (minword > 0):
-        break
-      else:
-        print("Input invalid! Make sure minimum lengths are shorter than maximums and are positive numbers!")
-        continue
-    print("This generator is a work in progress, so DON'T USE THIS PASSPHRASE!")
-    #todo: Use an actual dictionary source for words
-    passphrase = ""
-    #Actually builds the passphrase from user-defined separators and the variable dictionary
-    for i in range(random.randrange(minword, maxword)):
-      word = secrets.choice(dictionary)
-      passphrase = passphrase + word + separators[random.randint(0, len(separators)-1)]
-    passphrase = passphrase + str(random.randint(0, 9))
-    print(passphrase)
+      #Allow user to specify minimum and maximum lengths of passphrase and ensure specifications are valid
+      while 1==1:
+      #  Character length management is currently a disabled feature.
+      #  minchar = int(input("Minimum Character Length: "))
+      #  maxchar = int(input("Maximum Character Length: "))
+        minword = int(input("Minimum Word Length: "))
+        maxword = int(input("Maximum Word Length: "))
+        separators = input("Seperators (Multiple Allowed): ")
+        if (minword <= maxword) and (minword > 0):
+          break
+        else:
+          print("Input invalid! Make sure minimum lengths are shorter than maximums and are positive numbers!")
+          continue
+      while 1==1:
+        print("This generator is a work in progress, so DON'T USE THIS PASSPHRASE!")
+        #todo: Use an actual dictionary source for words
+       passphrase = ""
+       #Actually builds the passphrase from user-defined separators and the variable dictionary
+        for i in range(random.randrange(minword, maxword)):
+          word = secrets.choice(dictionary)
+          passphrase = passphrase + word + separators[random.randint(0, len(separators)-1)]
+       passphrase = passphrase + str(random.randint(0, 9))
+       print(passphrase)
+       print("""Would you like to...
+        1) Generate another passphrase, or
+        2) Return to main option selection?""")
+        userchoice2 = input("(Please select 1 or 2): ")
+        if userchoice2 == "1":
+          continue
+        else:
+          break
   elif userchoice == "2":
-    print("""Would you like to...
-    1) Add or remove entries,
-    2) View the generator's vocabulary, or
-    3) Return to the main option select?""")
-    userchoice2 = input("(Please select 1, 2, or 3): ")
-    if userchoice2 == "1":
-      #todo add vocab editor
-      print("This feature is not yet implemented. Sorry!")
-      continue
-    elif userchoice2 == "2":
-      for i in dictionary:
-        print(i)
-      continue
-    else:
-      continue
+    while 1==1:
+      print("""Would you like to...
+      1) Add or remove entries,
+      2) View the generator's vocabulary, or
+      3) Return to main option selection?""")
+      userchoice2 = input("(Please select 1, 2, or 3): ")
+      if userchoice2 == "1":
+        #todo add vocab editor
+        print("This feature is not yet implemented. Sorry!")
+        continue
+      elif userchoice2 == "2":
+        for i in dictionary:
+          print(i)
+        continue
+      else:
+        break
   else:
     break
